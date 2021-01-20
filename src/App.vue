0cee7c1429d55c913f2f160e93e8895a6d9ad056
fardisaz <amoozadeh73@yahoo.com>
2021-01-20 17:35:50 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 
+
 export default {
   name: "App",
   components: {
@@ -17,12 +18,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './index.css';
 </style>
